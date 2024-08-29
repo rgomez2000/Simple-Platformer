746f96b5565cea45a34034c858eac6ce15dbb013
rgomez2000 <83112451+rgomez2000@users.noreply.github.com>
2024-08-29 01:55:41 -0700

    Updated script.js
    Added collision logic functionality. Now the cube will have collision with the platform on all sides.

--- script.js
+++ script.js
@@ -71,6 +71,8 @@ class Platform {
   }
 }
 
+
+
 const player = new Player();
 
 const platformPositions = [
@@ -120,7 +122,33 @@ const animate = () => {
     }
   }
 
+  platforms.forEach((platform) => {
+    const collisionDetectionRules = [
+      player.position.y + player.height <= platform.position.y,
+      player.position.y + player.height + player.velocity.y >= platform.position.y,
+      player.position.x >= platform.position.x - player.width / 2,
+      player.position.x <=
+        platform.position.x + platform.width - player.width / 3,
+    ];
 
+    if (collisionDetectionRules.every((rule) => rule)) {
+      player.velocity.y = 0;
+      return;
+    }
+
+    const platformDetectionRules = [
+      player.position.x >= platform.position.x - player.width / 2,
+      player.position.x <=
+        platform.position.x + platform.width - player.width / 3,
+      player.position.y + player.height >= platform.position.y,
+      player.position.y <= platform.position.y + platform.height,
+    ];
+
+    if (platformDetectionRules.every(rule => rule)) {
+      player.position.y = platform.position.y + player.height;
+      player.velocity.y = gravity;
+    };
+  });
 }
 
 
